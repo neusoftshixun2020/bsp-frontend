28c91ea5f0d40bc8e28c741ab61d36eb335a8024
happym <1320355349@qq.com>
2020-07-08 19:20:44 +0800

    finish product(2/2)

--- src/store/modules/product-category.js
+++ src/store/modules/product-category.js
@@ -1,24 +1,21 @@
-import {fetchList ,deleteRecord,addRecord,updateRecord,searchRecord,changeState } from '../../api/product-category'
+import {
+  fetchList,
+  deleteRecord,
+  addRecord,
+  updateRecord,
+  searchRecord,
+  changeState,
+  addAndUpdateProductCategory
+} from '../../api/product-category'
+import { getAllProductCategory } from '@/api/product-category'
 
 
 const productCategory = {
 
   actions: {
-    FetchList ({commit}) {
-      return new Promise((resolve, reject) => {
-        fetchList().then(response => {
-          commit('')
-          resolve(response)
-        }).catch(error => {
-          console.log("获取失败");
-          reject(error)
-        })
-
-      })
-    },
-    DeleteRecord({commit},PRC_ID){
+    DeleteRecord({commit},data){
       return new Promise((resolve,reject)=>{
-        deleteRecord(PRC_ID).then(response=>{
+        deleteRecord(data).then(response=>{
           commit('')
           resolve(response)
         }).catch(error=>{
@@ -65,6 +62,28 @@ const productCategory = {
     ChangeState({commit},para){
       return new Promise((resolve,reject)=>{
         changeState(para.PRC_ID, para.state).then(response=>{
+          commit('')
+          resolve(response)
+        }).catch(error=>{
+          console.log("操作失败");
+          reject(error);
+        })
+      })
+    },
+    addAndUpdateProductCategory({commit}, form) {
+      return new Promise((resolve,reject)=>{
+        addAndUpdateProductCategory(form.productCategory, form.operationFlag).then(response=>{
+          commit('')
+          resolve(response)
+        }).catch(error=>{
+          console.log("操作失败");
+          reject(error);
+        })
+      })
+    },
+    getAllProductCategory({commit}) {
+      return new Promise((resolve,reject)=>{
+        getAllProductCategory().then(response=>{
           commit('')
           resolve(response)
         }).catch(error=>{
